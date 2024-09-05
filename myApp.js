1ec98305a9f523250a4e72cc65637aa17a15a075
Aiden Ramgoolam <aidenramgoolam@Aidens-MacBook-Pro.local>
2024-09-05 12:37:44 -0400

    M

--- myApp.js
+++ myApp.js
@@ -1,18 +1,7 @@
 let express = require('express');
 let app = express();
 
-
-app.get('/', (req, res) => {
-    res.send('Hello World');
-  });
-  
-  const PORT = 3001;
-  app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}/`);
-  });
-
-
-
+console.log('Hello World');
 
 
 
